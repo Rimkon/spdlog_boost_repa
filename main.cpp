1a8dfdb4b2cbd71796c93db4751b527e4ebbea82
Rimkon <37692739+Rimkon@users.noreply.github.com>
2020-06-28 00:02:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 
 int main ()
 {
-    spdlog::info("good day");
+//    spdlog::info("good day");
+    
+     auto logger = spdlog::stdout_logger_mt("console");
+  logger->info("version {} was started", version());
+
+    
+    
 }
